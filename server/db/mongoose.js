0ba855f63ce6c6843ab624b7bafa8d9e6161ac12
SenthilKumar Ramasamy <senthilsource@gmail.com>
2017-10-29 23:13:37 +0000

    Added Patch functionality

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
- mongoose.connect("mongodb://senthil:senthil@ds139585.mlab.com:39585/nodejsdb" || "mongodb://localhost:27017/TodoApp");
+ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp");
 
  //mongoose.connect("mongodb://localhost:27017/TodoApp");
 
