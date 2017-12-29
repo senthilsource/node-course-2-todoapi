30c233feb6dca8b2754bfc459abf060d8879ebc4
SenthilKumar Ramasamy <senthilsource@gmail.com>
2017-12-29 21:48:05 +0000

    Added Login

--- server/models/users.js
+++ server/models/users.js
@@ -69,6 +69,25 @@ UserSchema.statics.findByToken = function (token) {
   });
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+  var user = this;
+
+  return user.findOne({ email }).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }   
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if(res){
+          resolve(user);
+        }else{
+          reject();
+        }               
+      });
+    });
+  });
+};
+
 UserSchema.pre("save", function (next) {
   var user = this;
 
@@ -85,6 +104,7 @@ UserSchema.pre("save", function (next) {
     next();
   }
 });
+
 
 var userModel = mongoose.model('user', UserSchema);
 
--- server/server.js
+++ server/server.js
@@ -46,6 +46,17 @@ app.post("/users", (req, res) => {
   });
 });
 
+app.post("/users/login", (req, res)=>{
+  var body = _.pick(req.body, ["email", "password"]);
+  userModel.findByCredentials(body.email, body.password).then((user)=>{
+    return user.generateAuthToken().then((token)=>{
+      res.header("x-auth", token).send(user);
+    });   
+   }).catch((e)=>{
+     res.status(400).send(e);
+   })
+});
+
 app.get("/todos", (req, res) => {
   todoModel.find().then((results) => {
     res.send(results);
