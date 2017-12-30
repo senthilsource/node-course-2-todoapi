28237048434db52fcad7aeb2934fe6b9da7bdf12
SenthilKumar Ramasamy <senthilsource@gmail.com>
2017-12-30 00:06:54 +0000

    Added Env Configuration

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,9 @@
-var env = process.env.NODE_ENV || 'Development';
+var env = process.env.NODE_ENV || 'development';
 
-if(env=='Development'){
-
-}else if(env=='test'){
-  
+if(env==='test' || env==='development'){
+    var config = require("./config.json");
+    var envConfig = config[env];
+    Object.keys(envConfig).forEach((key)=>{
+        process.env[key] = envConfig[key];
+    });
 }
